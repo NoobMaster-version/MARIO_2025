88dadfbff24698c52f485d0be8ce2b3196f85ce9
Pathak <kartikeypathak.imp@gmail.com>
2025-03-17 19:38:37 +0530

    spawned robot in gazebo on ros2 jazzy

--- 4_simulation_gazebo/launch/basic_gazebo.launch.py
+++ 4_simulation_gazebo/launch/basic_gazebo.launch.py
@@ -35,7 +35,8 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
-        )
+        ),
+        launch_arguments={'gz_args': '--verbose -r'}.items()
     )    
     # model launch
     mario = IncludeLaunchDescription(
@@ -47,3 +48,4 @@ def generate_launch_description():
         gazebo,
         mario
     ])
+
