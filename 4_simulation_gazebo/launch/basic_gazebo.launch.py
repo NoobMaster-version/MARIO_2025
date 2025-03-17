f6cec47260ca8769b4c6d3dfeeb2661accee8662
NoobMaster-version <kartikeypathak.imp@gmail.com>
2025-03-17 12:12:29 +0530

    added support for ros2 jazzy

--- 4_simulation_gazebo/launch/basic_gazebo.launch.py
+++ 4_simulation_gazebo/launch/basic_gazebo.launch.py
@@ -1,54 +1,51 @@
 #!/usr/bin/python3
 
-# -*- coding: utf-8 -*-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_prefix
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
     pkg_mario_bot = get_package_share_directory('simulation_gazebo')
-   
+
+    # We get the whole install dir
     description_package_name = "simulation_gazebo"
     install_dir = get_package_prefix(description_package_name)
-    
-    gazebo_models_path = os.path.join(pkg_mario_bot, 'models')
-    
-    if 'GZ_SIM_RESOURCE_PATH' in os.environ:
-        os.environ['GZ_SIM_RESOURCE_PATH'] = os.environ['GZ_SIM_RESOURCE_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
-    else:
-        os.environ['GZ_SIM_RESOURCE_PATH'] = install_dir + "/share" + ':' + gazebo_models_path
-    
-    if 'GZ_SIM_SYSTEM_PLUGIN_PATH' in os.environ:
-        os.environ['GZ_SIM_SYSTEM_PLUGIN_PATH'] = os.environ['GZ_SIM_SYSTEM_PLUGIN_PATH'] + ':' + install_dir + '/lib'
-    else:
-        os.environ['GZ_SIM_SYSTEM_PLUGIN_PATH'] = install_dir + '/lib'
 
-    print("GAZEBO MODELS PATH=="+str(os.environ["GZ_SIM_RESOURCE_PATH"]))
-    print("GAZEBO PLUGINS PATH=="+str(os.environ["GZ_SIM_SYSTEM_PLUGIN_PATH"]))
-    
-    gazebo = IncludeLaunchDescription(
+    # Set the path to the world file
+    world_file = PathJoinSubstitution([FindPackageShare(description_package_name), 'worlds', 'empty.sdf'])
+
+    # Launch Gazebo Harmonic with ROS 2 bridge
+    gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
         ),
         launch_arguments={
             'gz_args': '-r empty.sdf'
-        }.items()
+        }.items(),
     )
-    
+
+    # Launch the robot
     mario = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_mario_bot, 'launch', 'mario.launch.py'),
         )
     )
-    
+
+    # Bridge between ROS 2 and Gazebo
+    bridge = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_bridge.launch.py'),
+        )
+    )
+
     return LaunchDescription([
-        gazebo,
-        mario
+        gz_sim,
+        mario,
+        bridge
     ])
--- 4_simulation_gazebo/launch/mario.launch.py
+++ 4_simulation_gazebo/launch/mario.launch.py
@@ -2,29 +2,27 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument,  ExecuteProcess, RegisterEventHandler
-from launch.substitutions import Command
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch.event_handlers import (OnProcessStart, OnProcessExit)
 from launch_ros.descriptions import ParameterValue
-import random
-
 
 def generate_launch_description():
     ####### DATA INPUT ##########
     urdf_file = 'manipulator.urdf'
-    #xacro_file = "box_bot.xacro"robot_description
     package_description = "simulation_gazebo"
     ####### DATA INPUT END ##########
-    config = os.path.join( get_package_share_directory('simulation_gazebo'),
-    'config',
-    'manipulator.yaml'
+    config = os.path.join(
+        get_package_share_directory('simulation_gazebo'),
+        'config',
+        'manipulator.yaml'
     )
 
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
     print("Fetching URDF ==>")
 
-    robot_description_content = Command(['xacro ',robot_desc_path])
+    robot_description_content = Command(['xacro ', robot_desc_path])
 
     robot_description = {"robot_description": robot_description_content}
     control_node = Node(
@@ -52,30 +50,31 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': ParameterValue(Command(['xacro ',robot_desc_path]), value_type=str)}],
+        parameters=[{'use_sim_time': True, 'robot_description': ParameterValue(Command(['xacro ', robot_desc_path]), value_type=str)}],
         output="screen"
     )
 
     # Position and orientation
-    # [X, Y, Z]
     position = [0.0, 0.0, 0.0]
-    # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
-    # Base Name or robot
     robot_base_name = "mario"
-    # Spawn ROBOT
+
+    # Spawn robot using Gazebo Harmonic's service
     spawn_robot = Node(
         package='ros_gz_sim',
         executable='create',
         name='spawn_entity',
-        output='screen',
-        arguments=['-entity', robot_base_name,
-                    '-x', str(position[0]), '-y', str(position[1]
-                                                        ), '-z', str(position[2]),
-                    '-R', str(orientation[0]), '-P', str(orientation[1]
-                                                        ), '-Y', str(orientation[2]),
-                    '-topic', '/robot_description'
-                    ]
+        arguments=[
+            '-entity', robot_base_name,
+            '-topic', '/robot_description',
+            '-x', str(position[0]),
+            '-y', str(position[1]),
+            '-z', str(position[2]),
+            '-R', str(orientation[0]),
+            '-P', str(orientation[1]),
+            '-Y', str(orientation[2])
+        ],
+        output='screen'
     )
 
     delay_joint_state_broadcaster_spawner_after_spawn_robot = RegisterEventHandler(
